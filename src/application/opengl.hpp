74a8a38d5c18b15e3fcdb6d388f5958d0c585420
hilman <hilmanbeyri@gmail.com>
2015-01-19 19:04:16 -0500

    - messy code to display color triangle

--- src/application/opengl.hpp
+++ src/application/opengl.hpp
@@ -7,6 +7,8 @@
 #ifndef _462_OPENGL_HPP_
 #define _462_OPENGL_HPP_
 
+#include <GL\glew.h>
+
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
--- src/p1/project.cpp
+++ src/p1/project.cpp
@@ -36,6 +36,11 @@ OpenglProject::~OpenglProject()
     // They will cause undefined behavior (probably a crash, but perhaps worse).
 }
 
+//data for rendering
+static GLuint meshVboId, meshIndexId, colorId;
+GLuint vertId, idxId;
+//---
+
 /**
  * Initialize the project, doing any necessary opengl initialization.
  * @param camera An already-initialized camera.
@@ -47,7 +52,44 @@ bool OpenglProject::initialize( Camera* camera, Scene* scene )
     // copy scene
     this->scene = *scene;
 
-    // TODO opengl initialization code and precomputation of mesh/heightmap
+	//TODO : should glut be inited here ?
+
+	glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+
+	GLenum err = glewInit();
+	if (err != GLEW_OK)
+	{
+		//glewInit failed
+		std::cerr << "glewInit failed, something is seriously wrong." << std::endl;
+		return false;
+	}
+
+	//data test
+	GLfloat vertices[] = { 0.0, 0.0, 0.0, 1.0, 1.0, 1.0 };
+	GLuint indices[] = { 0, 1, 2 };
+	//
+
+	//glGenBuffers(1, &meshVboId);
+	//glGenBuffers(1, &meshIndexId);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, meshVboId);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshIndexId);
+
+	////glBufferData(GL_ARRAY_BUFFER, sizeof(this->scene.mesh.vertices) * this->scene.mesh.num_vertices, this->scene.mesh.vertices, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+	////glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(this->scene.mesh.triangles) * this->scene.mesh.num_triangles, this->scene.mesh.triangles, GL_STATIC_DRAW);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Create buffers
+	//glGenBuffers(1, &vertId);
+	glGenBuffers(1, &idxId);
+	//// Bind buffers to modify/render them
+	//glBindBuffer(GL_ARRAY_BUFFER, vertId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxId);
+	//// Load data into buffers
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     return true;
 }
 
@@ -78,7 +120,39 @@ void OpenglProject::update( real_t dt )
  */
 void OpenglProject::render( const Camera* camera )
 {
-    // TODO render code
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, meshVboId);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, meshIndexId);
+
+	////glVertexPointer(3, GL_FLOAT, 0, 0);
+	////glDrawElements(GL_TRIANGLES, scene.mesh.num_triangles * 3, GL_UNSIGNED_INT, 0);*/
+
+	//glEnableClientState(GL_VERTEX_ARRAY);
+	//glVertexPointer(2, GL_FLOAT, 0, 0);
+	//glDrawElements(GL_TRIANGLES, 1, GL_UNSIGNED_INT, 0);
+	//glDisableClientState(GL_VERTEX_ARRAY);
+	//glDisableClientState(GL_VERTEX_ARRAY);
+	//glColor3f(0.0f, 0.0f, 1.0f);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vertId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxId);
+
+	GLfloat colors[] = { 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f };
+	GLfloat vertices[] = { 0.0, 0.0, 0.0, 1.0, 1.0, 1.0 };
+	GLuint indices[] = { 0, 1, 2 };
+
+	glEnableClientState(GL_COLOR_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	
+	glVertexPointer(2, GL_FLOAT, 0, vertices);
+	glColorPointer(3, GL_FLOAT, 0, colors);
+	
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+	//glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices) / sizeof(GLfloat));
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
 }
 
 } /* _462 */
